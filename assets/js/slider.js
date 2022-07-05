1ef2f1d2dd8a1f963f37e4be77980fda6cb520e7
TechnocraticManipulator <contact@maximebenoit.fr>
2022-07-05 14:05:04 +0200

    final version small img

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,64 +2,43 @@ const sculptures = document.getElementsByClassName("keen-slider__slide");
 
 let slider = new KeenSlider("#artwork-slider", {
   breakpoints: {
-    "(min-width: 400px)": {
-      slides: { perView: 4, spacing: 10 },
+    "(min-width: 768px)": {
+      slides: { perView: 2, spacing: 0 },
     },
+    "(min-width: 1025px)": {
+      slides: { perView: 3, spacing: 0 },
+    },
+    "(min-width: 1680px)": {
+      slides: { perView: 4, spacing: 0 },
+    },
+    "(min-width: 1920px)": {
+      slides: { perView: 5, spacing: 0 },
+    }
   },
   slides: { perView: 1 },
 });
-
-// if (
-//   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-//     navigator.userAgent
-//   )
-// ) {
-//   slider = new KeenSlider("#artwork-slider", {
-//     breakpoints: {
-//       "(min-width: 400px)": {
-//         slides: { perView: 4, spacing: 5 },
-//       },
-//       "(min-width: 1000px)": {
-//         slides: { perView: 4, spacing: 10 },
-//       },
-//     },
-//     slides: { perView: 1 },
-//   });
-// } else {
-//   slider = new KeenSlider("#artwork-slider", {
-//     breakpoints: {
-//       "(min-width: 400px)": {
-//         slides: { perView: 4, spacing: 5 },
-//       },
-//       "(min-width: 1000px)": {
-//         slides: { perView: 4, spacing: 10 },
-//       },
-//     },
-//     slides: { perView: 1 },
-//   });
-// }
 
 
 for (let index = 0; index < sculptures.length; index++) {
   sculptures[index].addEventListener("mouseenter", e => {
     let currentEl = e.target;
-    let currentElChild = currentEl.firstChild;
-    // const sizeInfo = document.createElement("div");
-    // sizeInfo.id = "size-info";
+    let currentElChild = currentEl.childNodes[1];
+    console.log(currentElChild);
+    const sizeInfo = document.createElement("div");
+    sizeInfo.id = "size-info";
 
-    // let artworkSizing = currentElChild.dataset.size;
-    // const sizeInfoTxt = document.createElement("p");
-    // sizeInfoTxt.innerHTML = artworkSizing;
+    let artworkSizing = currentElChild.dataset.size;
+    const sizeInfoTxt = document.createElement("p");
+    sizeInfoTxt.innerHTML = artworkSizing;
     // currentElChild.style.mixBlendMode = "normal";
 
-    // currentEl.appendChild(sizeInfo);
-    // sizeInfo.appendChild(sizeInfoTxt);
+    currentEl.appendChild(sizeInfo);
+    sizeInfo.appendChild(sizeInfoTxt);
   });
   sculptures[index].addEventListener("mouseleave", e => {
     let currentEl = e.target;
-    let currentElChild = currentEl.firstChild;
-    // const lastEl = document.getElementById("size-info");
-    // lastEl.remove();
-    //currentElChild.style.mixBlendMode = "multiply";
+    const lastEl = document.getElementById("size-info");
+    lastEl.remove();
+    // currentElChild.style.mixBlendMode = "multiply";
   });
 }
